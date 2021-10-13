e5cacd0c7bc42212a29e4d98b4bfcc2751dbb398
Himanshu Rajvanshi <himanshu.rajvanshi@synoriq.in>
2021-10-13 10:19:09 +0530

    Initial commit

---
+++ src/main/java/com/project/microservices/springcloudconfigserverv2/SpringCloudConfigServerV2Application.java
@@ -0,0 +1,13 @@
+package com.project.microservices.springcloudconfigserverv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerV2Application.class, args);
+	}
+
+}
